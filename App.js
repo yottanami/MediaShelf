9fdc352889b270c06230fc5fc1b08072e96b8743
yottanami <yottanami@gmail.com>
2018-12-25 01:47:42 +0330

    create simple App.js for test

--- App.js
+++ App.js
@@ -1,47 +1,31 @@
 import React, {Component} from 'react';
+import { AppRegistry, Text } from 'react-native';
+import  ApolloClient from 'apollo-boost';
+import  HttpLink from 'apollo-boost';
+import { InMemoryCache} from 'apollo-cache-inmemory';
 
-import {AppRegistry, View, Text} from 'react-native';
-import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-
-import gql from "graphql-tag";
-
-const cache = new InMemoryCache();
 
 
-const client = new ApolloClient({
-  uri: "http://127.0.0.1:3000/graphiql",
-  link: HttpLink,
-  cache
-});
+export default class extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-client
-  .query({
-    query: gql`
-             {
-                categories {
-  	           title
-	       }
-             }
-    `
-  })
-  .then(result => console.log('result'))
-  .catch(function(error) {
-    console.log('There has been a problem with your fetch operation: ' + error.message);
-    throw error;
-  });
-;
 
-export default class App extends Component {
   render() {
+    const cache = new InMemoryCache();
+    const url =  new HttpLink({uri: "https://127.0.0.0:3000/graphql"});
+    const client = new ApolloClient(
+      {
+        uri: url,
+        cache
+      }
+    );
     return (
       <ApolloProvider client={client}>
-        <div>
-          <h2>My first Apollo app </h2>
-        </div>
+
       </ApolloProvider>
     );
   }
