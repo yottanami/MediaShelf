3a9e86a58f783c3d9616e9cab144769e788960ac
yottanami <yottanami@gmail.com>
2019-01-30 16:37:33 +0330

    Clean up files

--- App.js
+++ App.js
@@ -1,39 +1,21 @@
 import React, {Component} from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 import  HttpLink from 'apollo-boost';
-import { InMemoryCache} from 'apollo-cache-inmemory';
 
-import  ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-
-
-import Category from './Category';
-import { categoryReducer } from './Reducers';
-
+import GraphqlProvider from './src/Components/GraphqlProvider';
+//import Category from './src/Screens/Category';
 export default class extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    const cache = new InMemoryCache();
-    //const url =  new HttpLink({uri: "http://172.16.6.146:3000/graphql"});
-    const client = new ApolloClient(
-      {
-        uri: "http://172.16.6.146:3000/graphql"
-        //cache
-      }
-    );
-
-    const store = applyMiddleware()(createStore);
-
     return (
-      <ApolloProvider client={client} store={store}>
+      <GraphqlProvider>
         <View>
-          <Category/>
+
         </View>
-      </ApolloProvider>
+      </GraphqlProvider>
     );
   }
 }
