57454d0e4c437423a13315be4ae068a7b807e13e
yottanami <yottanami@gmail.com>
2019-06-01 17:59:25 +0430

    Add carousel

--- src/Screens/Categories.js
+++ src/Screens/Categories.js
@@ -12,7 +12,9 @@ import gql from "graphql-tag";
 import Setting from "../Configs/settings";
 import { colors, fontSize, styles } from "../Styles/styles";
 import NavigationService from '../Configs/NavigationService';
+import { CustomCarousel } from "../Components/CustomCarousel";
 import Layout from '../Components/Layout';
+import Carousel, {pagination} from 'react-native-snap-carousel';
 
 const CategoryListQuery = gql`
 {
@@ -24,40 +26,107 @@ id
 }
 `;
 
-export default () => (
 
-  <Layout>
-    <Text style={styles.title}>Categories</Text>
-    <Query query={CategoryListQuery}>
-      {({ loading, error, data }) => {
-        if (loading) return <ActivityIndicator color={colors.teal} />;
-        if (error) return <Text>OH OH {`Error: ${error}`}</Text>;
+export default class Categories extends Component {
+  constructor(props) {
+    super(props);
 
-        return (
-          <View>
+    this.state = {
+      carousel_images: [
+        {
+          title: " "
+        }, {
+          title: " "
+        }, {
+          title: " "
+        }
+      ]
+    };
+  }
 
-            {data.categories
-             .map(({ title, image, id }, idx, rateArr) => (
-               <Card style={styles.card} key={title}>
-                 <CardItem cardBody>
-                   <Image
-                     style={{ width: 600, height: 150 }}
-                     source={{uri: Setting.serverMainPath +image.thumb.url}}
-                     onPress={() => NavigationService.navigate('Posts', {categoryId: id})}
-                   />
-                 </CardItem>
-                 <CardItem >
-                   <Text
-                     onPress={() => NavigationService.navigate('Posts', {categoryId: id})}>{title}</Text>
-                 </CardItem>
-               </Card>
+  _renderItem ({item, index, currentIndex}) {
+    const self = this;
+    return (
+      <View>
+        <Text>
+          {item.title}
+        </Text>
+      </View>
+    );
+  }
 
-             ))}
+  get pagination () {
+    const { entries, activeSlide } = this.state;
+    return (
+      <Pagination
+        dotsLength={entries.length}
+        activeDotIndex={activeSlide}
+        containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 5,
+          marginHorizontal: 8,
+          backgroundColor: 'rgba(255, 255, 255, 0.92)'
+        }}
+        inactiveDotStyle={{
+          // Define styles for inactive dots here
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+      />
+    );
+  }
+  render() {
+
+    return (
+      <Layout>
+        <Text style={styles.title}>Categories</Text>
+
+        <Carousel
+          ref={(c) => { this._carousel = c; }}
+          renderItem={this._renderItem}
+          sliderWidth={499}
+          itemWidth={400}
+          data={this.state.carousel_images}
+          autoplay={true}
+          autoplayDelay={500}
+          autoplayInterval={3000}
+        />
+        { this.pagination }
+
+        <Query query={CategoryListQuery}>
+          {({ loading, error, data }) => {
+            if (loading) return <ActivityIndicator color={colors.teal} />;
+            if (error) return <Text>OH OH {`Error: ${error}`}</Text>;
+
+            return (
+              <View>
+
+                {data.categories
+                 .map(({ title, image, id }, idx, rateArr) => (
+                   <Card style={styles.card} key={title}>
+                     <CardItem cardBody>
+                       <Image
+                         style={{ width: 600, height: 150 }}
+                         source={{uri: Setting.serverMainPath +image.thumb.url}}
+                         onPress={() => NavigationService.navigate('Posts', {categoryId: id})}
+                       />
+                     </CardItem>
+                     <CardItem >
+                       <Text
+                         onPress={() => NavigationService.navigate('Posts', {categoryId: id})}>{title}</Text>
+                     </CardItem>
+                   </Card>
+
+                 ))}
 
 
-          </View>
-        );
-      }}
-    </Query>
-  </Layout>
-);
+              </View>
+            );
+          }}
+        </Query>
+      </Layout>
+    );
+  }
+};
